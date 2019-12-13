127cf136ca793ebf144ab4c95c5c60e711dd74eb
uuko <omkkqq@gmail.com>
2019-12-13 16:58:31 +0800

    "home page 小修"

--- app/src/main/java/com/example/mymail/CategoryActivity.java
+++ app/src/main/java/com/example/mymail/CategoryActivity.java
@@ -3,17 +3,35 @@ package com.example.mymail;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.material.navigation.NavigationView;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+
 public class CategoryActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private RecyclerView categoryrecylceview;
+
+    //////////////////////////
+    private RecyclerView categoryRecycleView;
+    private CategoryAdapter categoryAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +42,7 @@ public class CategoryActivity extends AppCompatActivity implements NavigationVie
         String title=getIntent().getStringExtra("Categoryname");
         getSupportActionBar().setTitle(title);
         getSupportActionBar().setDisplayShowTitleEnabled(true);
+
 
     }
     @Override
